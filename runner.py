a8b679edec17a9ab0038fb44c6e3e9c421784de2
azhang44 <60955354+azhang44@users.noreply.github.com>
2021-11-14 22:59:37 -0800

    Add files via upload

--- runner.py
+++ runner.py
@@ -1,2 +1,38 @@
 from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+@app.route('/hello/<name>') 
+def hello_name(name):
+    return 'Hello %s!' % name 
+
+@app.route("/")
+def login():
+    return render_template('login.html')
+
+@app.route('/admin', methods =['GET'])
+def admin():
+    return render_template('admin.html')
+
+@app.route("/student/view")
+def student_view():
+    return render_template('student-view-classes.html')
+
+@app.route("/student/edit")
+def student_edit():
+    return render_template('student-edit-classes.html')
+
+@app.route("/teacher/view")
+def teacher_view():
+
+    return render_template('teacher-view-classes.html')
+
+@app.route("/teacher/view/<class_name>")
+def teacher_edit(class_name):
+    return render_template('teacher-view-class-details.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+    
