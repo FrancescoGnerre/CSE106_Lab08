96ff97334003b1357d2caffa393f71845ef121c7
Alex Holt <alexholt54@gmail.com>
2021-11-14 23:02:28 -0800

    Update runner.py
    Creates database

--- runner.py
+++ runner.py
@@ -2,6 +2,9 @@ from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///class-enrollment.sqlite"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
 @app.route('/hello/<name>') 
 def hello_name(name):
@@ -34,5 +37,3 @@ def teacher_edit(class_name):
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-    
