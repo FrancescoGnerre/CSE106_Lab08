ef2ab93882a008278ca4a92ec9ad7b88e98f3e02
Alex Holt <alexholt54@gmail.com>
2021-11-16 13:11:21 -0800

    Fixed spacing

--- runner.py
+++ runner.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///class-enrollment.sqlite"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
-login_manager = LoginManager() 
-login_manager.init_app(app) 
+login_manager = LoginManager()
+login_manager.init_app(app)
 login_manager.login_view = 'login'
 app.secret_key = 'keep it secret, keep it safe'
 db = SQLAlchemy(app)
