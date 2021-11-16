b10f481edfb1d9b94cd32bd4ea863bc95a04701e
Alex Holt <alexholt54@gmail.com>
2021-11-16 13:07:51 -0800

    Revert "Fix Spacing so Pycharm does not yell at me for wrong formatting"
    This reverts commit cbbea37d9597e9d3d38a8800ab57104f819cafb7.

--- runner.py
+++ runner.py
@@ -1,29 +1,43 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
+from flask.helpers import url_for
 from flask_admin import Admin
-from flask_login import login_required, logout_user, login_user, current_user
+from flask_login import login_required, logout_user, login_user, current_user, LoginManager, UserMixin
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///class-enrollment.sqlite"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+login_manager = LoginManager() 
+login_manager.init_app(app) 
+login_manager.login_view = 'login'
+app.secret_key = 'keep it secret, keep it safe'
 db = SQLAlchemy(app)
 
+@login_manager.user_loader 
+def load_user(user_id): 
+    return Users.query.filter_by(user_id = user_id).first()
+
 # Many to many relationship table with Users and Classes
-enrollment = db.Table('enrollment',
-            db.Column("user_id", db.Integer, db.ForeignKey("users.user_id")),
-            db.Column("class_id", db.Integer, db.ForeignKey("classes.class_id")),
-            db.Column("grade", db.Integer, default = 0) # This line may need fixing...
-            )
+class Enrollment(db.Model):
+    __tablename__ = "Enrollment"
+    users_id = db.Column(db.ForeignKey("Users.user_id"), primary_key = True)
+    classes_id = db.Column(db.ForeignKey("Courses.class_id"), primary_key = True)
+    grade = db.Column(db.Integer, nullable = False)
+
+    def __init__(self, user_id, classes_id, grade):
+        self.users_id = user_id
+        self.classes_id = classes_id
+        self.grade = grade
 
 # User table
-class Users(db.Model):
+class Users(UserMixin, db.Model):
+    __tablename__ = "Users"
     user_id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String, nullable = False)
     name = db.Column(db.String, nullable = False)
     password = db.Column(db.String, nullable = False)
     acct_type = db.Column(db.Integer, nullable = False) # 0 - Student, 1 - Teacher, 2 - Admin
-    enrollment = db.relationship("Classes", secondary = enrollment, backref = db.backref("enroll", lazy = "dynamic"))
 
     def __init__(self, username, name, password, acct_type):
         self.username = username
@@ -34,8 +48,12 @@ class Users(db.Model):
     def check_password(self, password):
         return self.password == password
 
+    def get_id(self):
+        return self.user_id
+
 # Classes Table
-class Classes(db.Model):
+class Courses(db.Model):
+    __tablename__ = "Courses"
     class_id = db.Column(db.Integer, primary_key = True)
     class_name = db.Column(db.String, nullable = False)
     teacher = db.Column(db.String, nullable = False)
@@ -51,59 +69,78 @@ class Classes(db.Model):
         self.capacity = capacity
 
 # Login
-@app.route("/")
+@app.route("/", methods = ["GET", "POST"])
 def login():
-    return render_template('login.html')
+    if request.method == "POST":
+        data = request.get_json()
+        user = Users.query.filter_by(username=data['username']).first() 
+        if user is None or not user.check_password(data['password']): 
+            return redirect(url_for('login')) 
+        login_user(user)
+        if user.acct_type == 0:
+            return url_for('student_view')[1:]
+        elif user.acct_type == 1:
+            return url_for('teacher_view')[1:]
+        else:
+            return url_for('admin')[1:]
+    else:   
+        return render_template('login.html')
 
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
 
 # Admin
 @app.route('/admin')
+@login_required
 def admin():
     return render_template('admin.html')
 
-
 @app.route('/admin/C', methods =['GET', 'POST'])
+@login_required
 def admin_create():
-    return render_template('admin.html')
-
+    return 0
 
 @app.route('/admin/R', methods =['GET'])
+@login_required
 def admin_read():
-    return render_template('admin.html')
-
+    return 0
 
 @app.route('/admin/U', methods =['GET', 'PUT'])
+@login_required
 def admin_update():
-    return render_template('admin.html')
-
+    return 0
 
 @app.route('/admin/D', methods =['DELETE'])
+@login_required
 def admin_delete():
-    return render_template('admin.html')
-
+    return 0
 
 # Student
-@app.route("/student/view")
+@app.route("/student")
+@login_required
 def student_view():
     return render_template('student-view-classes.html')
 
-
 @app.route("/student/edit")
+@login_required
 def student_edit():
     return render_template('student-edit-classes.html')
 
-
 # Teacher
 @app.route("/teacher/view")
+@login_required
 def teacher_view():
     return render_template('teacher-view-classes.html')
 
-
 @app.route("/teacher/view/<class_name>")
+@login_required
 def teacher_edit(class_name):
     return render_template('teacher-view-class-details.html')
 
-
-# run
+# Run
 if __name__ == "__main__":
+    # db.create_all() # Only need this line if db not created
     app.run(debug=True)
